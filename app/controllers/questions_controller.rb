cfdcf04ff75b349b46b25c5df8f3ae0b3f142d02
Corey Ellis <coreypozzatiellis@gmail.com>
2022-12-03 11:27:33 +1100

    updated Gemfile

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    if @question == 'I am going to work'
+    if @question.downcase == 'i am going to work'
       @response = 'Great!'
     elsif @question.end_with?('?')
       @response = 'Silly question, get dressed and go to work!'
